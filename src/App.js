200a786772e3ad14565abbf912f9f1dce41ce17c
Bohdan Shcherbak <b.szczerbak@gmail.com>
2022-01-31 21:20:04 +0100

    Print out countries and their flags

--- src/App.js
+++ src/App.js
@@ -1,16 +1,48 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import axios from 'axios';
 
 const App = () => {
   const [countries, setCountries] = useState([]);
-  (async () => {
-    const response = await axios.get('https://api.covid19tracking.narrativa.com/api/countries');
-    setCountries(response.data.countries.map(({ id, name }) => ({ id, name })));
-  })();
+  useEffect(async () => {
+    const covidResponse = await axios.get('https://api.covid19tracking.narrativa.com/api/countries');
+    const countriesResponse = await axios.get('https://restcountries.com/v3.1/all');
+    setCountries(covidResponse.data.countries.map(({ id, name }) => {
+      const country = countriesResponse.data.filter((country) => (
+        country.name.common === name
+        || (country.altSpellings && country.altSpellings.includes(name))
+      ))[0];
+      if (country) {
+        return {
+          id,
+          name,
+          flag: country.flag,
+          flagSVG: country.flags.svg,
+          continent: country.continents[0],
+        };
+      }
+      return undefined;
+      // console.log(name);
+      // return {
+      //   id,
+      //   name,
+      // };
+    }).filter((country) => country !== undefined));
+  }, []);
 
   return (
     <div>
-      {countries.map(({ id, name }) => (<p key={id}>{name}</p>))}
+      {countries.map(({
+        id, name, flag, flagSVG, continent,
+      }) => (
+        <div key={id}>
+          <img style={{ width: '200px' }} src={flagSVG} alt={`flag of ${name}`} />
+          <p>
+            {name}
+            {flag}
+            {continent}
+          </p>
+        </div>
+      ))}
     </div>
   );
 };
