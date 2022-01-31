dda0c634e1e8cc3bf557103d7bc7dfb847b09fce
Bohdan Shcherbak <b.szczerbak@gmail.com>
2022-01-31 13:13:06 +0100

    Remove all bilerplate files

--- src/App.js
+++ src/App.js
@@ -1,27 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import axios from 'axios';
 
-function App() {
+const App = () => {
+  const [countries, setCountries] = useState([]);
+  (async () => {
+    const response = await axios.get('https://api.covid19tracking.narrativa.com/api/countries');
+    setCountries(response.data.countries.map(({ id, name }) => ({ id, name })));
+  })();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          <code>src/App.js</code>
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {countries.map(({ id, name }) => (<p key={id}>{name}</p>))}
     </div>
   );
-}
+};
 
 export default App;
--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,5 +1,6 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import '@testing-library/jest-dom';
+import App from '../App';
 
 test('renders learn react link', () => {
   render(<App />);
